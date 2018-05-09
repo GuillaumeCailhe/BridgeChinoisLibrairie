39ef7fe77fa3cc6b7e3acd6996f640a3c3ad16e5
guillaume <g.cailhe@gmail.com>
2018-05-09 15:00:16 +0200

    Renommage des classes.

---
+++ src/LibrairieReseau/MessageString.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LibrairieReseau;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+/**
+ *
+ * @author Pepefab
+ */
+public class MessageString extends Message {
+    
+    String donnees;
+    
+    public MessageString(CodeMessage code, DataInputStream fluxEntrant) throws IOException {
+        super(code,fluxEntrant);
+        int taille = fluxEntrant.readByte();
+        donnees = "";
+        for(int i=0; i<taille; i++){
+            donnees += fluxEntrant.readChar();
+        }
+    }
+    
+}
